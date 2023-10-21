9badd82275c42588cc0ad0ea0d5519d6c9dd3b47
lg4tin <lg4tin@gmail.com>
2023-10-21 14:56:45 -0400

    Solve fibsRecursice function

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 function fibsIteration(num) {
   let fibs = [];
-  let total = 0;
   for (let i = 0; i < num; i++) {
     if (i === 0 || i === 1) {
       fibs.push(i);
@@ -15,16 +14,7 @@ function fibsIteration(num) {
 
 console.log(fibsIteration(8));
 
-function fibsRecursive(num) {
-  let fibs = [];
 
-  if (num === 0) return fibs;
-  if (num === 1 || num === 2) fibs.push(num)
-  if (num >= 2) {
-    return fibs.push(fibsRecursive(num - 1) + fibsRecursive(num - 2));
-  }
 
-  return fibs
-}
-
-console.log(fibsRecursive(2))
+const fibsRecursion = (num) => num >= 2 ? fibsRecursion(num - 1) + fibsRecursion(num - 2) : num;
+console.log(fibsRecursion(15));
