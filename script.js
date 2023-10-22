8655cb17f420bd1097fff22e9ce5fe1766bcb58f
lg4tin <lg4tin@gmail.com>
2023-10-21 20:39:45 -0400

    Solve mergeSort function

--- script.js
+++ script.js
@@ -15,6 +15,36 @@ function fibsIteration(num) {
 console.log(fibsIteration(8));
 
 
-
+let arr = []
 const fibsRecursion = (num) => num >= 2 ? fibsRecursion(num - 1) + fibsRecursion(num - 2) : num;
-console.log(fibsRecursion(15));
+console.log(fibsRecursion(8));
+
+let array = [4,1,3,6,7,5,2,8,19,2,1,3,78,3]
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array;
+  }
+
+  let left = array.splice(0, array.length / 2);
+  let right = array;
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let arr = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+
+  const sortedArray = [...arr, ...left, ...right];
+  return sortedArray;
+}
+ 
+console.log(mergeSort(array))
